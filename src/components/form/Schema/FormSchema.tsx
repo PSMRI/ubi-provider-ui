63c041f78684fea7cce67c5ee3605982ea149158
Sagar Takle <sagarkoshti1990@gmail.com>
2024-10-04 16:01:45 +0530

    fix:sonar issue

--- src/components/form/Schema/FormSchema.tsx
+++ src/components/form/Schema/FormSchema.tsx
@@ -6,29 +6,17 @@ import { JSONSchema7 } from "json-schema"; // Use this for the schema type
 import validator from "@rjsf/validator-ajv6";
 // Define your JSON Schema for the form
 const schema: JSONSchema7 = {
-  title: "General Information",
+  title: "RJSF From",
   type: "object",
-  required: ["firstName", "lastName", "age"],
+  required: ["name"],
   properties: {
-    benefitName: { type: "string", title: "Benefit Name" },
-    benefitProvider: { type: "string", title: "Benefit Provider" },
-    benefitSponsor1: { type: "string", title: "Benefit Sponsor-1" },
-    sponsor1Entity: { type: "string", title: "Sponsor-1 Entity Type" },
-    sponsor1Share: { type: "string", title: "Sponsor-1 Share (Percentage)" },
-    benefitSponsor2: { type: "string", title: "Benefit Sponsor-2" },
-    sponsor2Entity: { type: "string", title: "Sponsor-2 Entity Type" },
-    sponsor2Share: { type: "string", title: "Sponsor-2 Share (Percentage)" },
-    description: { type: "string", title: "Description" },
+    name: { type: "string", title: "Benefit Name" },
   },
 };
 
 // Form Component
 const FormSchema: React.FC = () => {
-  return (
-    <Form schema={schema} validator={validator}>
-      <div />
-    </Form>
-  );
+  return <Form validator={validator} schema={schema} />;
 };
 
 export default FormSchema;
