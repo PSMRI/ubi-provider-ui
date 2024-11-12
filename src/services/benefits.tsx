cf99c5a0b4f467bcb419080c7c143a4cffa340da
namita-25 <112607821+namita-25@users.noreply.github.com>
2024-11-12 11:59:02 +0530

    Task #229463 API integration for create and update benefit (#23)
    * Task #229075 Solve sonar issue and remove T & C from login
    
    * Task #229075 Solve sonar issue
    
    * Task #229075 text changes
    
    * Task #229075 fix comment by manoj sir
    
    * Task #229075 fix: add height to modal,loading position,lang constant
    
    * Task #229075 - fix ChartWithErrorBoundary
    
    * Task #229075 - fix Typo,add const in format and alphabatically,add one liner HTML
    
    * Task #229075 - fix Typo mistake
    
    * Task#229472 checking Digit API to see CORS issue
    
    * Task#229472 change URL name from proxy
    
    * Task#229472 error useEffect solve
    
    * Checking digit API on Dev
    
    * add lock file
    
    * Add error handling and validate key uniqueness, let to const
    
    * change it from coderabit suggestion shows error so revert
    
    * change proxy url to check on dev
    
    * change base url
    
    * Correct the digit API call and add env var for proxy
    
    * Task #229463 API integration for create and update benefit

--- src/services/benefits.tsx
+++ src/services/benefits.tsx
@@ -1,9 +1,99 @@
 import { tableData } from "../utils/dataJSON/BenefitSummary";
+import axios from "axios";
 
 export const getAll = async () => {
   //   const response = await axios.get(`http://localhost:3001/api/benefits`);
 
   return tableData;
 };
+interface BenefitRequestInfo {
+  apiId: string;
+  ver?: string | null;
+  ts?: string | null;
+  action?: string | null;
+  did?: string | null;
+  key?: string | null;
+  msgId: string;
+  authToken: string;
+  userInfo: {
+    id: number;
+    uuid: string;
+    userName: string;
+    name: string;
+    mobileNumber: string;
+    emailId: string;
+    type: string;
+    active: boolean;
+    tenantId: string;
+  };
+}
 
+interface SponsorData {
+  id: string;
+  benefitSponsor: string;
+  sponsorEntity: string;
+  sponsorShare: string;
+  type: string;
+}
+
+interface Benefit {
+  benefitName: string;
+  benefitProvider: string;
+  benefitDescription: string;
+  sponsors: SponsorData[];
+  status: string;
+}
+interface BenefitAmountCategory {
+  beneficiaryCaste: string;
+  beneficiaryType: string;
+  beneficiaryCategory: string;
+  beneficiaryAmount: number;
+}
+interface TermsAndCondition {
+  academicYear?: boolean;
+  failYear?: boolean;
+  deadlineDate?: string;
+  extendDeadlineDate: string;
+  validDate: string;
+  renewableApplicable: boolean;
+}
+interface BenefitPayload {
+  RequestInfo: BenefitRequestInfo;
+  Benefit: Benefit;
+}
+
+interface BenefitAmountCategoryPayload {
+  RequestInfo: BenefitRequestInfo;
+  Benefit: Benefit;
+  BenefitAmountCategory: BenefitAmountCategory[];
+}
+interface BenefitTermsAndCondition {
+  RequestInfo: BenefitRequestInfo;
+  Benefit: Benefit;
+  TermsAndCondition: TermsAndCondition;
+}
 export default { getAll };
+
+export const createBenefitForm = async (payload: BenefitPayload) => {
+  try {
+    const response = await axios.post(`/benefits/v1/_create`, payload);
+    console.log(response.data);
+    return response?.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateForm = async (
+  payload:
+    | BenefitPayload
+    | BenefitAmountCategoryPayload
+    | BenefitTermsAndCondition
+) => {
+  try {
+    const response = await axios.post(`/benefits/v1/_update`, payload);
+    console.log(response.data);
+    return response?.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
