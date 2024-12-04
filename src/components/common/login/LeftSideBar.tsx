36a33ca78d88ddbc5364f1d9a71cadbf5e90d6f9
namita-25 <namita_g@tekditechnologies.com>
2024-12-04 11:55:57 +0530

    Task #230243 logo,label chnages from login and header and browser tab icon

--- src/components/common/login/LeftSideBar.tsx
+++ src/components/common/login/LeftSideBar.tsx
@@ -1,6 +1,6 @@
 import { HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-import Logo from "../../../assets/Images/GOM.png";
+import Logo from "../../../assets/Images/Frame.png";
 
 export default function LeftSideBar() {
   const { t } = useTranslation();
@@ -15,33 +15,7 @@ export default function LeftSideBar() {
     >
       <HStack>
         <Image src={Logo} />
-        <VStack align={"start"}>
-          <Text
-            fontSize="24px"
-            fontWeight="400"
-            color={"white"}
-            textAlign="left"
-          >
-            {t("LEFTSIDE_CONTENT_HEADER_COMPANY_NAME")}
-          </Text>
-          <Text
-            fontSize="16px"
-            fontWeight="400"
-            color={"#000000"}
-            textAlign="left"
-            bg={"#D9D9D980"}
-          >
-            {t("LEFTSIDE_CONTENT_RIGHT_SUBTITLE")}
-          </Text>
-          <Text
-            fontSize="16px"
-            fontWeight="400"
-            color={"white"}
-            textAlign="left"
-          >
-            {t("LEFTSIDE_CONTENT_RIGHT_TEXT")}
-          </Text>
-        </VStack>
+        <VStack align={"start"}></VStack>
       </HStack>
     </VStack>
   );
