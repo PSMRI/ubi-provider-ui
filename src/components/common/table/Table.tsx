448f666c63cde620d18cbb43af3c281279e80305
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-06-19 10:19:18 +0530

    Task #242257 feat: replace Chakra UI Table with ka-table in DocumentList component

--- src/components/common/table/Table.tsx
+++ src/components/common/table/Table.tsx
@@ -3,8 +3,8 @@ import "ka-table/style.css";
 import { Table } from "ka-table";
 import { memo } from "react";
 
-const App = (props: any) => {
+const TableWrapper = (props: any) => {
   return <Table rowKeyField={"name"} {...props} />;
 };
 
-export default memo(App);
+export default memo(TableWrapper);
